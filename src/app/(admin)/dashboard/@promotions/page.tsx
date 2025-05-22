c1f3912dffa61d5bca40029749824f77b0777095
Kostenko Andrey <andrey@Mac.fibertel.com.ar>
2025-05-22 10:49:51 -0300

    update company & promo

--- src/app/(admin)/dashboard/@promotions/page.tsx
+++ src/app/(admin)/dashboard/@promotions/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { getPromotions } from '@/lib/api';
 import SummaryTable from '@/app/сomponents/summary-table';
 import SummaryTableHeader from '@/app/сomponents/summary-table-header';
 import SummaryTableCell from '@/app/сomponents/summary-table-cell';
 import DashboardCard from '@/app/сomponents/dashboard-card';
+import { getPromotions } from '@/lib/api';
 
 export interface PageProps {}
 
+export const dynamic = 'force-dynamic';
 export default async function Page({}: PageProps) {
   const data = await getPromotions();
 
